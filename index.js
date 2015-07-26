4721caf2cb394c3406361039aa3d70b656599e2e
ECR <[echenran@nyu.edu]>
2015-07-25 21:01:23 -0400

    Deleted extraneous files and (painfully) merged all code

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 var connect = require('connect');
 var serveStatic = require('serve-static');
-
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var path = require('path');
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/index1.html');
 });
 
 var users = 0;
@@ -27,17 +30,19 @@ io.on('connection',function(socket){
 	console.log('A user disconnected. '+users+' remaining.');
 	});
 
-	socket.on('new song', function(song){
-	songrequests.push([num_songrequests, song, 0]);
-	num_songrequests++;
-	console.log('"'+song+'" was added. Now '+num_songrequests+' requests: '+songrequests);
-	io.emit('add request', num_songrequests-1, song);
+	socket.on('new song', function(data_obj){
+		console.log(data_obj);
+		songrequests.push([num_songrequests, data_obj.name, 0, data_obj.link]);
+		num_songrequests++;
+		console.log('"'+data_obj.name+'" was added. Now '+num_songrequests+' requests: '+songrequests);
+		io.emit('add request', num_songrequests-1, data_obj);
 	});
 
 	socket.on('user approves', function(songnum){
 		songrequests[songnum][2]++;
 		console.log('a user approved a song: '+songrequests);
-		if (songrequests[songnum][2]==users) io.emit('song approved', songnum, songrequests[songnum][1]);
+		if (songrequests[songnum][2]==users)
+			io.emit('song approved', songnum, songrequests[songnum][1], songrequests[songnum][3]);
 	});
 
 	socket.on('user disapproves', function(songnum){
